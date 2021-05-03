f0a6648b0c3a87c150ce1b9f36ae25c6ef7f3228
Mooler0410 <814030466@qq.com>
2021-05-03 12:26:24 +0800

    final

--- backend/api/views.py
+++ backend/api/views.py
@@ -24,67 +24,74 @@ Userprofile = backend_models.Userprofile
 
 # Serve Vue Application
 index_view = never_cache(TemplateView.as_view(template_name='index.html')) # pragma: no cover
-
-
-
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
+from apscheduler.jobstores import base
 
+ 
 #开启定时工作
+
+def my_job():
+    print("hahaha")
+    '''
+    access to all User information and print all User's id and email address
+    '''
+    all_user = Userprofile.objects.all()
+    for user in all_user:
+        print(user.user_id, ' : ', user.email_address)
+        if user.sell_notify > 0:
+            opp_r = user.opportunity_cost
+            tax_s = user.short_tax_rate
+            tax_l = user.long_tax_rate  # by dafault.
+            full_horizon = user.invest_horizon
+            #send_email
+            for s in user.stocks.all():
+                s_code = s.code
+                yf_stock = yf.Ticker(s_code)
+                purchase_date = s.purchase_date
+                p0 = s.purchase_price 
+                pl = s.target_price
+                ps = yf_stock.history(period='1d')["Close"][0]
+                hs = ((timezone.now() - purchase_date).days) / 365     # in year
+                left_horizon = full_horizon - hs if (full_horizon - hs) > 0 else 0
+                close_date = yf_stock.history(period='1d').index[0].isoformat()[:10]
+                D = compute_D(tax_l, tax_s, pl, ps, p0, opp_r, left_horizon, hs)
+                if D < 0:
+                    if s._sended < 1.:
+                        expect_date = purchase_date + timedelta(days=user.invest_horizon*365//1)
+                        a_s = compute_return(ps, p0, tax_s, hs)
+                        a_l = compute_return(pl, p0, tax_l, left_horizon)
+                        mail_tempelate = 'Dear user {}:\n   Your stock {} has reached a price of {:.2f}.The return from holding the security until {}, \
+assuming it reached a price target of {:.2f}, would be {:.2%}. At the recent market price of {:.2f}, if you sell and invest the proceeds at your expected rate of return {:.2f}, \
+your return from selling now is {:.2%}. Therefore, we advise selling now.'.format(user.user_name, s.name, ps, purchase_date.isoformat()[:10], pl, a_l, ps, s.expect_return_rate, a_s)
+                        send_mail('Time to sell.',
+                                mail_tempelate,
+                                settings.EMAIL_HOST_USER,
+                                [user.email_address],
+                                fail_silently=False,)
+                    s._sended = 1.0 
+                else:
+                    s._sended = 0.0  
+                s.save()
+        else:
+            pass # No action
+
+
 try:
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
+    try:
+        job_ = scheduler.get_job('sell_notifyer')
+        if not job_ == None:
+            scheduler.add_job(my_job, 'interval', seconds=60, id='sell_notifyer')
     # 设置定时任务，选择方式为interval，时间间隔为10s
-    @register_job(scheduler,"interval", seconds=60)
-    def my_job():
-        '''
-        access to all User information and print all User's id and email address
-        '''
-        all_user = Userprofile.objects.all()
-        for user in all_user:
-            print(user.user_id, ' : ', user.email_address)
-            if user.sell_notify > 0:
-                opp_r = user.opportunity_cost
-                tax_s = user.short_tax_rate
-                tax_l = user.long_tax_rate  # by dafault.
-                full_horizon = user.invest_horizon
-                #send_email
-                for s in user.stocks.all():
-                    s_code = s.code
-                    yf_stock = yf.Ticker(s_code)
-                    purchase_date = s.purchase_date
-                    p0 = s.purchase_price 
-                    pl = s.target_price
-                    ps = yf_stock.history(period='1d')["Close"][0]
-                    hs = ((timezone.now() - purchase_date).days) / 365     # in year
-                    left_horizon = full_horizon - hs if (full_horizon - hs) > 0 else 0
-                    close_date = yf_stock.history(period='1d').index[0].isoformat()[:10]
-                    D = compute_D(tax_l, tax_s, pl, ps, p0, opp_r, left_horizon, hs)
-                    if D < 0:
-                        if s._sended < 1.:
-                            expect_date = purchase_date + timedelta(days=user.invest_horizon*365//1)
-                            a_s = compute_return(ps, p0, tax_s, hs)
-                            a_l = compute_return(pl, p0, tax_l, left_horizon)
-                            mail_tempelate = 'Dear user {}:\n   Your stock {} has reached a price of {:.2f}.The return from holding the security until {}, \
-assuming it reached a price target of {:.2f}, would be {:.2%}. At the recent market price of {:.2f}, if you sell and invest the proceeds at your expected rate of return {:.2f}, \
-your return from selling now is {:.2%}. Therefore, we advise selling now.'.format(user.user_name, s.name, ps, purchase_date.isoformat()[:10], pl, a_l, ps, s.expect_return_rate, a_s)
-                            send_mail('Time to sell.',
-                                    mail_tempelate,
-                                    settings.EMAIL_HOST_USER,
-                                    [user.email_address],
-                                    fail_silently=False,)
-                        s._sended = 1.0 
-                    else:
-                        s._sended = 0.0  
-                    s.save()
-            else:
-                pass # No action
-            
-    register_events(scheduler)
-    scheduler.start()
+    finally:
+        register_events(scheduler)
+        scheduler.start()
 except Exception as e:
     print(e)
     scheduler.shutdown()
+    
 
 
 def import_test_models():
